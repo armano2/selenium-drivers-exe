db69aff6b553c6e5af5849759341c8edbaf820ef
Armano <armano2@users.noreply.github.com>
2017-08-15 18:11:20 +0200

    Update package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const path = require('path')
+const os = require('os');
+const arch = os.arch();
 
 exports.versions = {
 	chromedriver: '2.31',
 	geckodriver: 'v0.18.0'
 }
-exports.geckodriver = path.join(__dirname, 'lib', process.platform === 'win32' ? 'geckodriver.exe' : 'geckodriver')
-exports.chromedriver = path.join(__dirname, 'lib', process.platform === 'win32' ? 'chromedriver.exe' : 'chromedriver')
+exports.geckodriver = path.join(__dirname, 'lib', arch, 'geckodriver.exe')
+exports.chromedriver = path.join(__dirname, 'lib', arch, 'chromedriver.exe')
